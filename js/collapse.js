1a445951ec64ce0449ec8845cfa670aab7449eb5
Loëvane Vircondelet <loevane.vircondelet@gmail.com>
2023-03-16 22:22:09 +0100

    fix read-more and clean presentation

--- js/collapse.js
+++ js/collapse.js
@@ -1,14 +1,22 @@
-const collapsible = document.getElementById("collapsible")
-const content_collapse = document.getElementById("content_collapse")
-
-
+const collapsible = document.getElementById("collapsible");
+const content_collapse = document.getElementById("content_collapse");
+console.log(`collabs`, collapsible);
 collapsible.onclick = (e) => {
-    console.log(`click!`)
-    if (content_collapse.hidden === false ){
-        content_collapse.hidden = true;
-        collapsible.textContent= "Lire plus";
-    }else{
-        content_collapse.hidden = false;
-        collapsible.textContent= "Voir moins";
-    }
+  if (collapsible.textContent == "Voir plus +") {
+    content_collapse.style = "display : inline";
+    collapsible.textContent = "Voir moins -";
+    console.log(`click! diplay none`);
+  } else {
+    content_collapse.style = "display : none";
+    collapsible.textContent = "Voir plus +";
+    console.log(`click! display inline`);
+  }
+
+  //   if (content_collapse.style === false) {
+  //     content_collapse.hidden = true;
+  //     collapsible.textContent = "Lire plus +";
+  //   } else {
+  //     content_collapse.hidden = false;
+  //     collapsible.textContent = "Voir moins -";
+  //   }
 };
