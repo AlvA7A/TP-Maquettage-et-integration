bda50df54492f5ef2576e00f6faf8575627f7c2d
Alv <valladier.alvin@gmail.com>
2023-03-17 12:30:29 +0100

    trucs

---
+++ js/anime.js
@@ -0,0 +1,10 @@
+/* Sélection des éléments HTML */
+let link = document.getElementById('link')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('ul')
+
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+link.addEventListener('click', function(e) {
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
